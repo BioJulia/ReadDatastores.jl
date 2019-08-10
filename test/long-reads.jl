ede4bf39adf658761380de9b210d88e867635141
Ben J. Ward <benjward@protonmail.com>
2019-08-10 11:33:24 +0100

    Increase test coverage for long read datastores

--- test/long-reads.jl
+++ test/long-reads.jl
@@ -11,10 +11,28 @@
     function check_round_trip(FQ)
         seqs = get_fastq_seqs(FQ)
         fq = open(FASTQ.Reader, FQ)
-        ds = LongReadDatastore(fq, "human-nanopore.prds", "human-nanopore", UInt64(0))
+        ds = LongReadDatastore(fq, "human-nanopore.lrds", "human-nanopore", UInt64(0))
+        ds2 = open(LongReadDatastore, "human-nanopore.lrds")
         ds_seqs = collect(ds)
-        return ds_seqs == seqs
+        ds2_seqs = collect(ds2)
+        return ds_seqs == seqs == ds2_seqs
     end
     
-    @test check_round_trip("human_nanopore_tester_2D.fastq")
+    @testset "Human oxford nanopore 2D consensus reads tester" begin
+        @test check_round_trip("human_nanopore_tester_2D.fastq")
+    
+        ds = open(LongReadDatastore, "human-nanopore.prds")
+        ds2 = open(LongReadDatastore, "human-nanopore.prds")
+    
+        @test index(ds) == index(ds2)
+        @test index(ds)[1] == index(ds2)[1]
+        @test firstindex(ds) == firstindex(ds2) == 1
+        @test lastindex(ds) == lastindex(ds2) == 10
+        
+        @test Base.IteratorSize(ds) == Base.IteratorSize(ds2) == Base.HasLength()
+        @test Base.IteratorEltype(ds) == Base.IteratorEltype(ds2) == Base.HasEltype()
+        @test Base.eltype(ds) == Base.eltype(ds2) == LongSequence{DNAAlphabet{4}}
+
+        
+    end
 end
