bfc15d8af279b0de5ab2e90cde3107abd63e0c74
Ben J. Ward <benjward@protonmail.com>
2020-04-19 00:47:48 +0100

    fixup

--- test/runtests.jl
+++ test/runtests.jl
@@ -9,9 +9,10 @@ include("linked-reads.jl")
 @testset "Error Messages" begin
     buf = IOBuffer()
     
-    Base.showerror(buf, ReadDatastores.DatastoreMagicError("myreads.prseq"))
-    
+    Base.showerror(buf, ReadDatastores.MissingMagicError("myreads.prseq"))
     @test String(take!(buf)) == "MissingMagicError: the file myreads.prseq does not appear to be a valid read datastore file, it does not begin with the expected magic bytes."
+    
+    
 end
 
 end # module
