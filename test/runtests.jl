9bb85c317b749145d65d238e358a46bb53bf9dda
Ben J. Ward <benjward@protonmail.com>
2020-04-19 00:55:04 +0100

    Add DatastoreTypeError message tester

--- test/runtests.jl
+++ test/runtests.jl
@@ -12,7 +12,8 @@ include("linked-reads.jl")
     Base.showerror(buf, ReadDatastores.MissingMagicError("myreads.prseq"))
     @test String(take!(buf)) == "MissingMagicError: the file myreads.prseq does not appear to be a valid read datastore file, it does not begin with the expected magic bytes."
     
-    
+    Base.showerror(buf, ReadDatastores.DatastoreTypeError{PairedReads{DNAAlphabet{2}}}("myreads.prseq", LongDS))
+    @test String(take!(buf)) == "DatastoreTypeError: myreads.prseq contains a long read datastore and cannot be opened as a PairedReads{DNAAlphabet{2}}"
 end
 
 end # module
